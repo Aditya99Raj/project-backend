42edf3f03e855855773f85980c78cfdf4df8177b
Aditya Raj <96101819+Aditya99Raj@users.noreply.github.com>
2022-05-26 20:36:43 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 const connect = () => {
 	return mongoose.connect(
-		"mongodb+srv://niru143:niru143@practice.dmokj.mongodb.net/MyuserDB?retryWrites=true&w=majority"
+		"mongodb+srv://aditya1:aditya1@practice.dmokj.mongodb.net/MyuserDB?retryWrites=true&w=majority"
 	);
 };
 
